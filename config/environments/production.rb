c4d09876a8cc711da1504949c366fd18febc17e5
Billy Groble <wgroble@tulane.edu>
2018-01-21 17:16:42 +0000

    added mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.action_mailer.perform_caching = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_caching = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
 
 
   config.action_mailer.delivery_method = :smtp
