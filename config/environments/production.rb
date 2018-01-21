1c34abda516366c1ac9f45fef681c0904dab17c4
Billy Groble <wgroble@tulane.edu>
2018-01-21 16:19:55 +0000

    contacts config fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,20 +80,19 @@ Rails.application.configure do
   
   
   config.action_mailer.perform_caching = false
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-address: 'smtp.gmail.com',
-port: 587,
-domain: 'gmail.com',
-user_name: 'extinguishcontact@gmail.com',
-password: 'socialsupportnetwork',
-authentication: 'plain',
-enable_starttls_auto: true 
-}
-config.action_mailer.default_url_options = { :host => "https://extinguish.io" }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'extinguishcontact@gmail.com',
+  password: 'socialsupportnetwork',
+  authentication: 'plain',
+  enable_starttls_auto: true 
+  }
+  config.action_mailer.default_url_options = { :host => "https://extinguish.io" }
 end
