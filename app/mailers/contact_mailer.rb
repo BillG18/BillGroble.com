7ab4865292a45b6c435a115662e5bfd774171089
Billy Groble <wgroble@tulane.edu>
2018-01-22 02:36:55 +0000

    added production credentials for mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ApplicationMailer
   #
   #   en.contact_mailer.contact_email.subject
   #
-  default from: "me@sandboxa4b150b113c0497eb214478e1841a302.mailgun.org.com"
+  default from: "postmaster@mail.billgroble.com"
   
   def contact_email(content)
     @email = content
