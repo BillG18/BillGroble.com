7b2506e7573413e78017f7a3a93b252095f66678
Billy Groble <wgroble@tulane.edu>
2018-05-02 14:29:11 +0000

    added snap+match privacy policy to link to

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   
   resources "contacts", only: [:new, :create]
   
+  get "/:page" => "static#show"
+  
   # match '/contacts',     to: 'contacts#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
